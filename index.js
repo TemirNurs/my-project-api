7119285039258bedf32d444fe25355e5cd677736
Acer <sputniknurs@gmail.com>
2021-12-25 10:03:21 +0600

    route for Authorization is done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -14,7 +15,8 @@ mongoose
   });
 
 app.use(express.json());
-app.use("/api/users", userRoute )
+app.use("/api/auth", authRoute );
+app.use("/api/users", userRoute );
 
 app.listen(5000, () => {
   console.log("backend server is running");
